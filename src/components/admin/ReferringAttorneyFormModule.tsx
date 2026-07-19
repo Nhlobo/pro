ee34b31b09c54544bf4c4b8cb3bf074b134c9165
Nhlovo <137711037+Nhlobo@users.noreply.github.com>
2026-07-19 19:17:41 +0200

    Update ReferringAttorneyFormModule.tsx

--- src/components/admin/ReferringAttorneyFormModule.tsx
+++ src/components/admin/ReferringAttorneyFormModule.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReferringAttorneyForm from '@/pages/ReferringAttorneyForm';
 
+/** Embeds the Referring Attorney form as the CRM's "New Attorney" tab. */
 const ReferringAttorneyFormModule: React.FC = () => {
   return (
-    <div className="mt-2 [&>div>div:first-child]:hidden [&>nav]:hidden">
-      <ReferringAttorneyForm />
+    <div className="mt-2">
+      <ReferringAttorneyForm embedded />
     </div>
   );
 };
